06be292508a1ca85198f43b8ede675bb0a5c44f9
lucaster <luca.cavagnoli@gmail.com>
2022-08-29 14:56:01 +0200

    logging debug info

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+console.log("debug info", {
+  "process.version": process.version,
+  LD_LIBRARY_PATH: process.env.LD_LIBRARY_PATH,
+  LD_PRELOAD: process.env.LD_PRELOAD,
+});
+
 const canvas = require('canvas');
 
-console.log('canvas', !!canvas);
+console.log("debug info", {
+  "canvas": !!canvas,
+});
 
 module.exports.hello = async () => Promise.resolve({
   status: 200,
   body: {
     message: 'Hello, World!',
-    cairoVersion: canvas?.cairoVersion,
+    cairoVersion: cairoVersion(),
   },
 });
 
@@ -14,6 +22,10 @@ module.exports.time = async () => Promise.resolve({
   status: 200,
   body: {
     time: new Date(),
-    cairoVersion: canvas?.cairoVersion,
+    cairoVersion: cairoVersion(),
   },
 });
+
+function cairoVersion() {
+  return canvas ? canvas.cairoVersion : 'no canvas';
+}
