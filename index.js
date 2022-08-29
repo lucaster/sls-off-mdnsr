a0b78ba50339fbccff8c092b4af234b934198322
lucaster <luca.cavagnoli@gmail.com>
2022-08-29 15:00:10 +0200

    refact

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-console.log("debug info", {
-  "process.version": process.version,
+console.log('debug info', {
+  'process.version': process.version,
   LD_LIBRARY_PATH: process.env.LD_LIBRARY_PATH,
   LD_PRELOAD: process.env.LD_PRELOAD,
 });
 
 const canvas = require('canvas');
 
-console.log("debug info", {
-  "canvas": !!canvas,
+console.log('debug info', {
+  canvas: !!canvas,
 });
 
 module.exports.hello = async () => Promise.resolve({
